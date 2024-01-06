7aec7b42aeb1abb4123237b8b6a671b9c54a3051
conqxeror <kadriwalimohammad@gmail.com>
2024-01-07 01:13:59 +0530

    authentication, create user, delete user, update user, many more ...

--- src/db/users.ts
+++ src/db/users.ts
@@ -19,7 +19,5 @@ export const getUserBySessionToken = (sessionToken: string)=> UserModel.findOne(
 });
 export const getUserId = (id: string) => UserModel.findById(id);
 export const createUser = (values: Record<string, any>) => new UserModel(values).save().then((user) => user.toObject());
-export const deleteUserById = (id: string) => UserModel.findOne({
-    _id : id
-})
+export const deleteUserById = (id: string) => UserModel.findOne({ _id : id })
 export const updateUserById = (id: string, values: Record<string, any>) => UserModel.findOneAndUpdate({id, values});
--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,10 @@ import cookieParser from 'cookie-parser';
 import compression from 'compression';
 import cors from 'cors';
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import router from './router';
+
+dotenv.config();
 
 const app = express();
 
@@ -18,13 +22,12 @@ app.use(bodyParser.json());
 
 const server = http.createServer(app);
 
-server.listen(8080, ()=>{
+server.listen(process.env.PORT, ()=>{
     console.log('Server running on http://localhost:8080/')
 });
 
-const MONGO_URL = process.env.MONGO_URL;
-
 mongoose.Promise = Promise;
-mongoose.connect(MONGO_URL);
+mongoose.connect(process.env.MONGO_URL);
 mongoose.connection.on('error', (error: Error) => console.log(console.error));
 
+app.use('/', router());
